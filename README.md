1d62d1abb05d567c1ffc5298c5831cf000b95fb9
thomaspickles <thomas.pickles@gmail.com>
2022-03-21 17:52:03 +0100

    Finished qs 2b and 2d, broadcast results to parents

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 
 ### Setup
 
-Start a number of terminals.
-`erl -sname alice -setcookie abc`
-`erl -sname bob -setcookie abc`
+Start a number of terminals, sharing a cookie.\
+`erl -sname alice -setcookie abc`\
+`erl -sname bob -setcookie abc`\
 `erl -sname eve -setcookie abc`
 
-Daisy chain them, starting with alice
-alice@unix: `ds:init().`
-bob@unix: `ds:init('alice@unix').`
+Daisy chain them, starting with alice\
+alice@unix: `ds:init().`\
+bob@unix: `ds:init('alice@unix').`\
 eve@unix: `ds:init('bob@unix').`
 
-To get own count:
-alice@unix:`ds:rpc(top,count).`
-To get another group's count:
-alice@unix:`ds:rpc('bob@unix',top,count).`
+### Queries
+
+**To query the system**\
+`count | number | list`\
+Own group: `ds:req_parent(count).`\
+Other group: `ds:req_parent('bob@unix',count).`\
+System-wide: `ds:get_for_system(count).`
+
+## TODO
+- CHECK BROADCAST WORKS FROM CHILD NODE
+- get rid of odd print message on same node when doing `ds:broadcast('hello',top,cascade).`
--- ds.erl
+++ ds.erl
@@ -1,37 +1,75 @@
 -module(ds).
 -compile(export_all).
 start_child() ->
-    Data = {1,rand:uniform(5)},
-    io:format("Hello, I'm bottom level proc ~p, from group [~p]. Ready to loop_child...\n", [self(),node()]),
+    List = [1,2,3],
+    Data = {1,rand:uniform(5),List},
+    io:format("Listening on child [~p:~p]...\n", [node(),self()]),
     loop_child(Data).
 
 start_parent() ->
-    io:format("I'm top proc ~p in group [~p]\n",[self(),node()]),
+    io:format("Listening on top [~p:~p]...\n", [node(),self()]),
     N = 2 + rand:uniform(3), % 3 to 5 inc
     Children=make_children(N),
     loop_parent(Children).
 
-sum_system(What) when (What == count) or (What == number) ->
+get_for_system(What) when (What == count) or (What == number) ->
     Groups = [node() | nodes()],
-    Values = [rpc(top,Group,What) || Group <- Groups],
-    lists:sum(Values).
+    Values = [req_parent(Group,What) || Group <- Groups],
+    Value = lists:sum(Values),
+    broadcast(Value,top,no_cascade), % let parents know
+    Value;
+get_for_system(What) when (What == list) ->
+    Groups = [node() | nodes()],
+    Lists = [req_parent(Group,list) || Group <- Groups],
+    List = lists:append(Lists),
+    broadcast(List,top,no_cascade),
+    List.
+
+notify_parent(Group, Message,ShouldCascade) ->
+    {top,Group} ! {self(), Message,ShouldCascade}.
+
+notify_child(Child, msg, Message) ->
+    Child ! {self(), msg, Message}.
 
 % {RegisteredName, NodeName}
-rpc(Pid, Node, Request) ->
-    {Pid,Node} ! {self(), Request},
+% await response
+req_parent(Group, Request) ->
+    {top,Group} ! {self(), Request},
     receive
         Response ->
             Response
     end.
 
 % send to same group
-rpc(Pid, Request) ->
-    rpc(Pid,node(),Request).
+req_parent(Request) ->
+    req_parent(node(),Request).
+
+% private - called by parent
+req_child(Child,Request) ->
+    Child ! {self(),Request},
+    receive
+        Response ->
+            Response
+    end.
+
+
+broadcast(Message,top,cascade)  ->
+    Groups = [node() | nodes()], % sends request to all groups
+    [notify_parent(Group,Message,cascade) || Group <- Groups];
+broadcast(Message,top,no_cascade)  ->
+    Groups = [node() | nodes()], % sends request to all groups
+    [notify_parent(Group,Message,no_cascade) || Group <- Groups];
+broadcast(Message,Pid,CascadeType) ->
+    % if a child, tell parent to do it
+    broadcast(Message,top,CascadeType).
+broadcast(Message,Pid) ->
+    broadcast(Message,top,cascade). % default broadcast is to cascade
 
 init() ->
     io:format("I'm main proc ~p in group [~p], The other groups are [~p]\n",[self(),node(),nodes()]),
-    register(top,spawn(node(), ds, start_parent, [])),
-    io:format("Goodbye from main proc. Top proc [~p] lives on\n",[top]).
+    Pid = spawn(node(), ds, start_parent, []),
+    io:format("Goodbye from main proc. Top proc [~p] lives on\n",[Pid]),
+    register(top,Pid).
 
 init(Node) ->
     net_adm:ping(Node), % make contact - it's transitive, can can daisy chain to connect all!
@@ -42,42 +80,42 @@ loop_parent(Children) ->
         {From,done} ->
             [Child ! {self(),done} || Child <- Children],
             io:format("Goodbye from top too ~p~n",[self()]),
-            From ! 'terminated';
-        {Pid,What} ->
-            Values = [get_value(Child,What) || Child <- Children],
-            % can generalise this to an accumulator for more general fold
-            Total =  lists:sum(Values),
-            % io:format("Accumulated group value is ~p~n",[Total]),
-            Pid ! Total,
-            loop_parent(Children)
-    end.
-
-get_value(Child,What) ->
-    % io:format("Group [~p] top sending request for ~p to ~p~n",[node(),What,Child]),
-    Child ! {self(),What},
-    receive
-        Value ->
-            Value
-    end.
+            From ! 'terminated',
+            exit('Finished');
+        {Pid,Message,cascade} ->
+            io:format("[~p] Got a msg: ~p~n",[self(),Message]),
+            [notify_child(Child,msg,Message) || Child <- Children]; % cascade to children
+        {Pid,Message,no_cascade} ->
+            io:format("[~p] Got a msg: ~p~n",[self(),Message]);
+        {Pid,list} ->
+            Lists = [req_child(Child,list) || Child <- Children],
+            Pid ! lists:append(Lists);
+        {Pid,count} ->
+            Values = [req_child(Child,count) || Child <- Children],
+            Pid ! lists:sum(Values) + 1; % don't forget to count myself!
+         {Pid,number} ->
+            Values = [req_child(Child,number) || Child <- Children],
+            Pid ! lists:sum(Values)
+    end,
+    loop_parent(Children).
 
 make_children(0) -> [];
 make_children(N) ->
     [spawn(node(), ds, start_child, []) | make_children(N-1)].
 
 loop_child(Data) ->
-    {Count, Value} = Data,
+    {Count, Value, List} = Data,
     receive
-        {Top, count} ->
-            % io:format("[~p] Received request for count from top~n",[self()]),
-            Top ! Count,
-            loop_child(Data);
-        {Top, number} ->
-            % io:format("[~p] Received request for number from top~n",[self()]),
-            Top ! Value,
-            loop_child(Data);
+        {Parent, count} ->
+            Parent ! Count;
+        {Parent, number} ->
+            Parent ! Value;
+        {Parent, list} ->
+            Parent ! List;
+        {Parent, msg, Message} ->
+            io:format("[~p] Got a msg: ~p~n",[self(),Message]);
         {_, done} ->
-            io:format("[~p] Terminating.  Goodbye~n",[self()])
-    end.
-
-whoami() ->
-    io:format("Node: [~p], Proc: [~p], Connected to: [~p]\n",[node(),self(),nodes()]).
+            io:format("[~p] Terminating.  Goodbye~n",[self()]),
+            exit('Finished')
+    end,
+    loop_child(Data).
